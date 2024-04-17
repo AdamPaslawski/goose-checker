265c6158a2488492a17f5758209bfcde87af3bd8
apaslaw <adam.paslawski@morningstar.com>
2024-04-17 12:15:07 -0600

    separate arg parsing from main method to enable testing more easily as well as non-cli usage

--- goose_checker/cli.py
+++ goose_checker/cli.py
@@ -1,18 +1,29 @@
 import argparse
-from concurrent.futures import ThreadPoolExecutor, as_completed
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from dotenv import load_dotenv
+from pydantic import BaseModel
 
-from goose_checker.chains import (base_checker, approve_or_deny,
+from goose_checker.chains import (approve_or_deny, base_checker,
                                   terraform_checker)
 from goose_checker.diff import get_git_diffs
 from goose_checker.goose_ascii import detected_ascii, goose_ascii
-from goose_checker.models import (AzureGooseChecker, GooseCheckerResponse,
-                                  OpenAIGooseChecker)
+from goose_checker.models import (AzureGooseChecker, GooseChecker,
+                                  GooseCheckerResponse, OpenAIGooseChecker)
 
 
-def run_checker(diff, goose_checker):
+class AppConfig(BaseModel):
+    branch: str = os.environ.get("GOOSE_CHECKER_BRANCH_NAME", "main")
+    model: str = os.environ.get("GOOSE_CHECKER_MODEL_NAME", "gpt-3.5-turbo")
+    provider: str = os.environ.get("GOOSE_CHECKER_PROVIDER", "openai")
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+def run_checker(diff, goose_checker: GooseChecker):
     if diff.file_name.endswith(".tf"):
         return terraform_checker(diff=diff, goose_checker=goose_checker)
     else:
@@ -29,58 +40,56 @@ def display_issues(responses: list[GooseCheckerResponse]):
             print("\n")
 
 
-def cli_main():
+def parse_args() -> AppConfig:
     parser = argparse.ArgumentParser(description="Checks if you goofed")
     parser.add_argument(
-        "--branch",
-        nargs="?",
-        default=os.environ.get("GOOSE_CHECKER_BRANCH_NAME", "main"),
-        help="The branch or commit to compare against",
+        "--branch", nargs="?", help="The branch or commit to compare against"
     )
     parser.add_argument(
         "--model",
         nargs="?",
-        default=os.environ.get("GOOSE_CHECKER_MODEL_NAME", "gpt-3.5-turbo"),
         help="LLM model to use, for Azure instances this is the deployment ID.",
     )
-
     supported_providers = ["azure", "openai"]
     parser.add_argument(
         "--provider",
         nargs="?",
-        default=os.environ.get("GOOSE_CHECKER_PROVIDER","openai"),
         choices=supported_providers,
         help=f"The cloud provider to use, choices are: {supported_providers}",
     )
 
     args = parser.parse_args()
+    return AppConfig(branch=args.branch, model=args.model, provider=args.provider)
 
-    print(f"Checking for geese by comparing to {args.branch}")
-    print(f"Using model: {args.model} provided by {args.provider}")
 
-    if args.provider == "azure":
+def main(config: AppConfig):
+    print(f"Checking for geese by comparing to {config.branch}")
+    print(f"Using model: {config.model} provided by {config.provider}")
+
+    if config.provider == "azure":
         goose_checker = AzureGooseChecker(
-            with_respect_to=args.branch, deployment_name=args.model
+            with_respect_to=config.branch, deployment_name=config.model
         )
-    elif args.provider == "openai":
+    elif config.provider == "openai":
         goose_checker = OpenAIGooseChecker(
-            with_respect_to=args.branch, model=args.model
+            with_respect_to=config.branch, model=config.model
         )
     else:
-        raise ValueError(f"Provider specified not supported: {args.provider}")
+        raise ValueError(f"Provider specified not supported: {config.provider}")
 
-    # Lets Cook
+    diffs = get_git_diffs(with_respect_to=config.branch)
 
-    diffs = get_git_diffs(with_respect_to=args.branch)
+    if len(diffs) == 0:
+        print(
+            f"Compared your current checked out branch to {config.branch} and found nothing to check"
+        )
+        return False
 
     all_responses = []
-    # Initialize ThreadPoolExecutor
     with ThreadPoolExecutor(max_workers=4) as executor:
-        # Submit tasks to the executor
         future_to_diff = {
             executor.submit(run_checker, diff, goose_checker): diff for diff in diffs
         }
-        # Retrieve and store results as they complete
         for future in as_completed(future_to_diff):
             diff = future_to_diff[future]
             try:
@@ -88,20 +97,22 @@ def cli_main():
                 all_responses.append(resp)
             except Exception as exc:
                 print(f"Failed to analyze {diff.file_name} due to {exc}")
-    if not all_responses:
-        return False
 
     approved, explanation = approve_or_deny(all_responses, goose_checker)
 
     if not approved:
-        print(goose_ascii)
-        print(detected_ascii)
+        print("Geese detected:")
         print(explanation)
-    
+        return approved
     else:
         print("Approved! No Geese detected.")
 
     return approved
+
+
+def cli_main():
+    config = parse_args()
+    main(config)
 
 
 if __name__ == "__main__":
